034b31ef90ce353c65a3321f44e35052524eb6e4
Vedant <vedantmohol18@gmail.com>
2025-06-06 16:17:46 +0530

    Connected with MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app = express();;
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.log(err);
+})
 
 app.listen(4000,()=>{
     console.log("Server is running on port 4000")
